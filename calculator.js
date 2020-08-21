249708207e295ca8d3f7750cba6614cf2e733bf4
Kavisha Modi <38474385+kavishamodi97@users.noreply.github.com>
2020-08-21 09:54:00 -0400

    Add files via upload
    Implement BMI Calculator

--- calculator.js
+++ calculator.js
@@ -18,7 +18,7 @@ res.sendFile(__dirname + "/index.html");
 //apply request-response over "Post" Method
 app.post("/",function(req,res){
 
-//need to cast in Number format otherwise it return in string format
+//need to cast in Number format otherwise it return in string format And Add Two Numbers
   var num1 =Number(req.body.num1);
 
   var num2= Number(req.body.num2);
@@ -26,6 +26,24 @@ app.post("/",function(req,res){
   var sum= num1 + num2;
 
   res.send("The Sum of Two Numbers are:- " +sum);
+
+});
+
+//apply request-response over "Get" method
+app.get("/bmicalculator",function(req,res){
+res.sendFile(__dirname +  "/bmiCalculator.html");
+});
+
+//apply request-response over "Post" Method And Calculate BMI
+app.post("/bmicalculator",function(req,res) {
+
+//parse into float values and return float type BMI values
+  var weight=parseFloat(req.body.weight);
+  var height=parseFloat(req.body.height);
+
+  var total=weight/(height*height);
+
+  res.send("BMI Calculation is:" + total);
 
 });
 
